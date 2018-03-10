c40a20c1aa5757558f829dd4a6966d69b3b0b128
rola <rola.elbakly@gmail.com>
2018-03-10 21:22:50 +0200

    insert almost done!

--- usingDB.sh
+++ usingDB.sh
@@ -72,7 +72,7 @@ checkType(){
            echo error:$input is Not a number
            return 1
        fi
-   elif [ $datatype='string' ]
+   elif [ "$datatype" = "string" ]
    then
        re='^[a-zA-Z]+$'
       if ! [[ $input =~ $re ]] ; then
@@ -83,6 +83,63 @@ checkType(){
    fi
 }
 
+checkPK(){
+  echo hello
+} 
+
+checkNull(){
+  input=$1
+  NullAllow=$2
+  if [ "$NullAllow" = "0" ]
+  then 
+      if [ "$input" = "0" ]
+      then 
+          echo Value can Not be null
+          return 1
+      fi
+
+  fi
+
+
+}
+checkUnique(){
+  input=$1
+  isUnique=$2
+  tname=$3
+  if [ $isUnique =  "1" ]
+  then
+      check=$( grep "$input" $dbName/$tname/records.txt )
+      if [ ! -z "$check" ]
+      then
+          echo Value must be unique
+          return 1
+      fi
+ 
+
+  fi
+}
+
+checkDefault(){
+   input=$1
+   default=$2
+
+   if [ $input = "default" ]
+   then
+       if [ $default = "none" ]
+       then
+           echo Field has no default
+           return 1
+       fi
+           
+
+   fi    
+
+
+ 
+
+}
+
+
 insertR(){
   #check table exists
   check1=$( grep "$1" "$dbName/$dbName.txt" )
@@ -92,6 +149,7 @@ insertR(){
       #count number of fields in table
       numfields=$(cat $dbName/$1/$1.txt |wc -l)
       counter=1
+      output=()
       echo Enter your record:
       read -a record
       if [ $numfields -eq ${#record[@]} ]
@@ -105,14 +163,36 @@ insertR(){
           #cut -d',' -f1 /tmp/restr.txt
           
           
-          checkType $i $( cut -d',' -f2 /tmp/restr.txt )
+          checkType $i $( cut -d',' -f2 /tmp/restr.txt ) 
+          tf=$?
+          echo $tf
           #checkPK $i $( cut -d',' -f3 /tmp/restr.txt )
-          #checkNull $i $( cut -d',' -f4 /tmp/restr.txt )
-          #checkUnique $i $( cut -d',' -f5 /tmp/restr.txt )
-          #checkDefault $i $( cut -d',' -f6 /tmp/restr.txt )
+
+          checkUnique $i $( cut -d',' -f4 /tmp/restr.txt ) $1
+          uf=$?
+          echo $uf
+
+          checkNull $i $( cut -d',' -f5 /tmp/restr.txt ) 
+          nf=$?
+          echo $nf
+         
+    
+          checkDefault $i $( cut -d',' -f6 /tmp/restr.txt )
+          df=$?
+          echo $df
           let "counter++"
-          
+          # check if all restrictions are met write it in array
+            let checkflags=$tf+$uf+$nf+$df
+            echo $checkflags
+            if [ $checkflags -eq 0 ]
+            then
+                output=(${output[@]} $i)
+            fi    
           done
+          if [ ${#output[@]} -eq $numfields ]
+          then
+              echo ${output[@]} >> $dbName/$1/records.txt
+          fi
       else
          echo Error,check your syntax!
       fi
@@ -151,7 +231,6 @@ dropT(){
   else
       echo no such table found
   fi  
-
 }
 
 descT(){
@@ -160,7 +239,7 @@ descT(){
   if [ $check1 ]
   then
      echo $1
-     echo "Field Name  Type  PK  Null  Unique Default  "
+     echo "Field Name  Type  PK  Null  Unique Default "
      awk 'BEGIN {FS=","}{print "Name: "$1;print "Datatype: "$2;print "PK: "$3;print "Null: "$4;print "Unique: "$5;print "Default: "$6}' $dbName/$1/$1.txt
 
      
@@ -211,9 +290,8 @@ do
         ;;
         *)
           echo no such command found check your syntax!
-        ;;
- esac
+  
+
+esac
+
 done
-
-
-
